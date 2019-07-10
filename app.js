9f5d7e7d61f4a88cf32cb34fb05c40a3aab9a840
momo <zhoubingyiya@gmail.com>
2019-07-10 16:48:38 +0800

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ new Vue({
     el: '#vue-app',
     data: {
         name: '周秉颐',
-        job: 'Web前端工程师',
+        job: 'Web 前端开发工程师',
         aboutMeItems: [{
                 aboutMe: '男 / 1996.5'
             },
@@ -40,21 +40,21 @@ new Vue({
         projects: {
             One: {
                 name: '购物网站',
-                content: '主要是使用 HTML 标记、div + CSS + JavaScript Dom 操作等前端 Web 技术进行网站的开发,通过 JavaScript(jQuery框架) 实现效果，如轮播图及回到顶部，导航下拉菜单及三级菜单实现。'
+                content: '使用 HTML 标记、div + CSS + JavaScript Dom 操作等前端 Web 技术进行网站的开发,通过 JavaScript(jQuery框架) 实现效果，如轮播图及回到顶部，导航下拉菜单及三级菜单实现。使用 Python/ Django 进行后端开发。'
             },
             Two: {
                 name: '报表系统',
-                content: '使用 HTML、CSS 完成页面基本布局,协助使用 Python/ Django 进行后端开发。用 JavaScript 实现在页面直接查找数据，不直接查找数据库,负责开发小组的工作调度、人员职能安排与分配、网站的功能策划与需求分析。'
+                content: '使用 HTML、CSS 完成页面基本布局,使用 Python/ Django 进行后端开发。用 JavaScript 实现在页面直接查找数据，不直接查找数据库,负责开发小组的工作调度、人员职能安排与分配、网站的功能策划与需求分析。'
 
             },
             Three: {
                 name: '新闻管理系统',
-                content: '负责编写详细需求分析和客户管理模块,使用 HTML、CSS 完成页面基本布局,协助使用 JSP 进行后端开发，实现了新闻添加、新闻修改、新闻删除等几大功能。'
+                content: '负责编写详细需求分析和客户管理模块,使用 HTML、CSS 完成页面基本布局,使用 JSP 进行后端开发，实现了新闻添加、新闻修改、新闻删除等几大功能。'
 
             },
             four: {
                 name: '房租数据分析',
-                content: '使用 python 爬取贝壳找房数据，使用 pandas 进行数据清理，describe 方法计算出最大值、 最小值、 平 均值等进行分析， 使用 Matplotlib 将得到的数据分析得到的数据表格进行可视化输出。'
+                content: '使用 Python 爬取贝壳找房数据，使用 pandas 进行数据清理，describe 方法计算出最大值、最小值、平均值等进行分析；使用 Matplotlib 将得到的数据分析得到的数据表格进行可视化输出。'
             },
         },
 
@@ -62,38 +62,29 @@ new Vue({
         skills: '技能',
         skillItems: [
             {
-                skill: '掌握 Web 前端开发基本技能，熟悉 W3C 标准'
+                skill: 'HTML、CSS、JavaScript 掌握 Web 前端开发基本技能，熟悉 W3C 标准'
             },
             {
-                skill: '熟知 JS 基本语法与知识，熟练使用原生 JavaScript 进行开发和交互'
+                skill: 'Python  '
             },
             {
-                skill: '掌握 HTML、CSS 等网页制作技术'
-            },
-            {
-                skill: '熟悉页面架构和布局'
-            },
-            {
-                skill: '了解 jQuery，Vue.js 等主流框架'
-            },
-            {
-                skill: '掌握 Python、Git、PhotoShop'
+                skill: 'Google、Git、PhotoShop'
             }
         ],
 
         // 自我评价
         selfEvaluation: '自我评价',
         selfItems: [{
-                self: '对互联网产品和Web技术有强烈兴趣，有优秀的学习能力和强烈的进取心'
+                self: '对互联网产品和Web技术有强烈兴趣，有优秀的学习能力和强烈的进取心。'
             },
             {
-                self: '开发过一些开源的小项目，比如使用 Python 将课表导入日历'
+                self: '开发过一些开源的小项目，比如使用 Python 将课表导入日历。'
             },
             {
-                self: '喜欢钻研各种新技术，经常到github学习代码'
+                self: '喜欢钻研各种新技术，经常到 Github 学习代码。'
             },
             {
-                self: '较强的事业心和责任心'
+                self: '较强的事业心和责任心。'
             }
         ],
 
